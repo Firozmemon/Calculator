eb9e9f25ced2bbff01043e5659cc16c38d766bf0
Firozmemon <firozmemon0@gmail.com>
2017-05-01 20:41:58 +0530

    Setting up Constraint Layout in UI

---
+++ app/src/main/java/com/firozmemon/mycalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.firozmemon.mycalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Check https://github.com/Gperez88/CalculatorInputView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
